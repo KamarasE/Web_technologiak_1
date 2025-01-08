49144bb9fc67404a2203822db8cdd2801eef11a0
KamarasE <91242919+KamarasE@users.noreply.github.com>
2025-01-08 14:41:05 +0100

    Add files via upload

---
+++ webtech1/scripts.js
@@ -0,0 +1,7 @@
+function textFunction() {
+  document.getElementById("erdekessegek").innerHTML = "Ide kerülnek az érdekességek";
+} 
+
+function validateForm() {
+   alert("The form was submitted");
+} 
